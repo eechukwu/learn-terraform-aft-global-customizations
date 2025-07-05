24df0af53640e395199a880a223bb7f69f792d29
Chukwu Emmanuel <eechukwu@gmail.com>
2025-07-05 20:43:15 +0100

    step3: add variables and locals for service-quota module

--- terraform/service_quotas.tf
+++ terraform/service_quotas.tf
@@ -1,36 +1,19 @@
 ############################
 # SINGLE QUOTA RESOURCE
 ############################
-resource "aws_servicequotas_service_quota" "sg_rules_us_east_1" {
-  provider     = aws.us_east_1
-  service_code = var.quota_service_code
-  quota_code   = var.quota_code
-  value        = var.default_target_quota_value
-}
+resource "aws_servicequotas_service_quota" "sg_rules" {
+  # Use region names as keys, then dynamically reference the provider
+  for_each = toset(local.target_regions)
 
-resource "aws_servicequotas_service_quota" "sg_rules_eu_west_2" {
-  provider     = aws.eu_west_2
-  service_code = var.quota_service_code
-  quota_code   = var.quota_code
-  value        = var.default_target_quota_value
-}
+  # Dynamic provider reference using the region name
+  provider = lookup({
+    "us-east-1"      = aws.us_east_1
+    "eu-west-2"      = aws.eu_west_2
+    "ap-southeast-1" = aws.ap_southeast_1
+    "us-west-2"      = aws.us_west_2
+    "ca-central-1"   = aws.ca_central_1
+  }, each.key)
 
-resource "aws_servicequotas_service_quota" "sg_rules_ap_southeast_1" {
-  provider     = aws.ap_southeast_1
-  service_code = var.quota_service_code
-  quota_code   = var.quota_code
-  value        = var.default_target_quota_value
-}
-
-resource "aws_servicequotas_service_quota" "sg_rules_us_west_2" {
-  provider     = aws.us_west_2
-  service_code = var.quota_service_code
-  quota_code   = var.quota_code
-  value        = var.default_target_quota_value
-}
-
-resource "aws_servicequotas_service_quota" "sg_rules_ca_central_1" {
-  provider     = aws.ca_central_1
   service_code = var.quota_service_code
   quota_code   = var.quota_code
   value        = var.default_target_quota_value
@@ -39,32 +22,18 @@ resource "aws_servicequotas_service_quota" "sg_rules_ca_central_1" {
 ############################
 # MATCHING DATA SOURCE
 ############################
-data "aws_servicequotas_service_quota" "live_us_east_1" {
-  provider     = aws.us_east_1
-  service_code = var.quota_service_code
-  quota_code   = var.quota_code
-}
+data "aws_servicequotas_service_quota" "live" {
+  for_each = toset(local.target_regions)
 
-data "aws_servicequotas_service_quota" "live_eu_west_2" {
-  provider     = aws.eu_west_2
-  service_code = var.quota_service_code
-  quota_code   = var.quota_code
-}
+  # Dynamic provider reference using the region name
+  provider = lookup({
+    "us-east-1"      = aws.us_east_1
+    "eu-west-2"      = aws.eu_west_2
+    "ap-southeast-1" = aws.ap_southeast_1
+    "us-west-2"      = aws.us_west_2
+    "ca-central-1"   = aws.ca_central_1
+  }, each.key)
 
-data "aws_servicequotas_service_quota" "live_ap_southeast_1" {
-  provider     = aws.ap_southeast_1
-  service_code = var.quota_service_code
-  quota_code   = var.quota_code
-}
-
-data "aws_servicequotas_service_quota" "live_us_west_2" {
-  provider     = aws.us_west_2
-  service_code = var.quota_service_code
-  quota_code   = var.quota_code
-}
-
-data "aws_servicequotas_service_quota" "live_ca_central_1" {
-  provider     = aws.ca_central_1
   service_code = var.quota_service_code
   quota_code   = var.quota_code
 }
@@ -74,10 +43,7 @@ data "aws_servicequotas_service_quota" "live_ca_central_1" {
 ############################
 output "security_group_quota_values" {
   value = {
-    "us-east-1"      = data.aws_servicequotas_service_quota.live_us_east_1.value
-    "eu-west-2"      = data.aws_servicequotas_service_quota.live_eu_west_2.value
-    "ap-southeast-1" = data.aws_servicequotas_service_quota.live_ap_southeast_1.value
-    "us-west-2"      = data.aws_servicequotas_service_quota.live_us_west_2.value
-    "ca-central-1"   = data.aws_servicequotas_service_quota.live_ca_central_1.value
+    for r in local.target_regions :
+    r => data.aws_servicequotas_service_quota.live[r].value
   }
 }
